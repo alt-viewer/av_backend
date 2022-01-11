97be38e85fd4d471df9bc3b90fea7afb41b7a948
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-11 08:00:17 +0000

    Short circuiting

--- matching/compare.py
+++ matching/compare.py
@@ -15,7 +15,13 @@ def item_ids(char: Character) -> List[str]:
 def compare(
     char1: Character, char2: Character, min_confidence: int = 70
 ) -> Optional[Match]:
-    confidence = ratio(item_ids(char1), item_ids(char2))
+    is1, is2 = item_ids(char1), item_ids(char2)
+    # Comparing 2 characters with no account items is not
+    # a useful comparison
+    if len(is1) == 0 or len(is2) == 0:
+        return None
+
+    confidence = ratio(is1, is2)
     if confidence > min_confidence:
         return Match(char1.id, char2.id, datetime.now(), confidence)
     return None
