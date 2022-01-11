a48080672ec195453908ee4b609810c8f96bcb0d
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-11 05:37:44 +0000

    optional path

--- queries/api_query.py
+++ queries/api_query.py
@@ -4,12 +4,13 @@ with open("service_id.txt") as f:
     service_id = f.readline().strip("\n")
 
 
-def query(path: str, params: dict = None, webhook: bool = False) -> URL:
+def query(path: str = None, params: dict = None, webhook: bool = False) -> URL:
     """Construct a PS2 API query."""
     query = params or {}
+    full_path = "/streaming" if webhook else "/get/ps2:v2/" + (path or "")
     return URL.build(
         scheme="wss" if webhook else "https",
         host="push.planetside2.com" if webhook else "census.daybreakgames.com",
-        path="/streaming" if webhook else "/get/ps2:v2/" + path,
-        query={"service_id": service_id, **query},
+        path=full_path,
+        query={"service-id": service_id, **query},
     )
