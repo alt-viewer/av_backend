81f417d076f7367012369ee26458642cba37b51f
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 04:41:04 +0000

    Using CharacterQueue

--- __main__.py
+++ __main__.py
@@ -1,11 +1,21 @@
 from aiohttp import ClientSession
-from listener import LoginListener, handle_characters
 import asyncio
+import toolz.curried
+from logging import getLogger
+
+from listener import LoginListener, CharacterQueue
+from queries import get_characters
+from database import push_db, log_db
+from logger import with_logger
 
 
 async def main():
+    char_logger = getLogger("characters")
     async with ClientSession() as session:
-        listener = LoginListener(session, handle_characters(session))
+        curried_getter = toolz.curry(get_characters)(session)
+        getter = with_logger(char_logger, "Requesting characters {1}")(curried_getter)
+        listener = LoginListener(session, CharacterQueue(getter, push_db))
+        asyncio.create_task(log_db())
         await listener.listen()
 
 
