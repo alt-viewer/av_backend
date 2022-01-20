94f30643916219745917bdb72104275ef465bdeb
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 01:59:25 +0000

    Logging wrapper

--- logger.py
+++ logger.py
@@ -1,4 +1,18 @@
 import logging
+from functools import wraps
 
 logging.basicConfig(level=logging.INFO)
 global_logger = logging.getLogger("global_logger")
+
+
+def with_logger(logger: logging.Logger):
+    def outer(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            output = func(*args, **kwargs)
+            logger.debug(f"{func} called with {args=}, {kwargs=}. {output=}")
+            return output
+
+        return inner
+
+    return outer
