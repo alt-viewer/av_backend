4b5173f9790aa797874aea9ae2a8dae5646ab4cd
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 21:42:35 +0000

    Fixed empty request

--- database/push_char.py
+++ database/push_char.py
@@ -1,4 +1,5 @@
 import toolz.curried as toolz
+from typing import Iterator
 
 from database.db_client import client
 from database.mutation import mutations
@@ -8,8 +9,8 @@ from entities import Character
 async def push_chars(chars: list[Character]) -> None:
     txn = client.txn()
     try:
-        req = mutations(txn, chars)
-        txn.async_do_request(req)
+        for char in chars:
+            txn.mutate(set_obj=char.json())
         txn.commit()
     finally:
         txn.discard()
