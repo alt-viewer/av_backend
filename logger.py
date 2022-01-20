c4ea986b8558043347eef69c16b2724262888489
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 04:38:14 +0000

    with_logger takes a format string

--- logger.py
+++ logger.py
@@ -5,12 +5,19 @@ logging.basicConfig(level=logging.INFO)
 global_logger = logging.getLogger("global_logger")
 
 
-def with_logger(logger: logging.Logger):
+def with_logger(
+    logger: logging.Logger,
+    format: str = "{0} called with args={1}, kwargs={2}. output={3}",
+):
+    """Wrap a function with logging.
+    The format will be filled with 0: func, 1: args, 2: kwargs, 3: output
+    """
+
     def outer(func):
         @wraps(func)
         def inner(*args, **kwargs):
             output = func(*args, **kwargs)
-            logger.debug(f"{func} called with {args=}, {kwargs=}. {output=}")
+            logger.debug(format.format(func, args, kwargs, output))
             return output
 
         return inner
