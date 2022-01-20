e26b86412da73660de0f737f460f80c9fb9faea7
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 10:17:09 +0000

    New startup script

--- dgraph/run_db.sh
+++ dgraph/run_db.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -p "8080:8080" -p "9080:9080" -p "8000:8000" -v ~/dgraph:/dgraph "dgraph/standalone:v21.12.0"
+docker run -it -p 5080:5080 -p 6080:6080 -p 8080:8080 -p 9080:9080 -p 8000:8000 -v ~/dgraph:/dgraph --name dgraph dgraph/standalone:v21.03.0
