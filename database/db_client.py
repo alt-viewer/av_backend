b5806ea1829c904a9f8706fdb43c0ec799dde41e
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 21:41:37 +0000

    Port works but ratel doesn't

--- database/db_client.py
+++ database/db_client.py
@@ -1,4 +1,4 @@
 import pydgraph as dgraph
 
-client_stub = dgraph.DgraphClientStub("localhost:8080")
+client_stub = dgraph.DgraphClientStub("localhost:9080")
 client = dgraph.DgraphClient(client_stub)
