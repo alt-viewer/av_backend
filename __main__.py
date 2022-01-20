b6cfe7d11643d8530e5e72a00534c933adfce226
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 04:59:33 +0000

    Gracceful exit

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ from aiohttp import ClientSession
 import asyncio
 import toolz.curried
 from logging import getLogger
+import sys
 
 from listener import LoginListener, CharacterQueue
 from queries import get_characters
@@ -10,13 +11,20 @@ from logger import with_logger
 
 
 async def main():
-    char_logger = getLogger("characters")
     async with ClientSession() as session:
-        curried_getter = toolz.curry(get_characters)(session)
-        getter = with_logger(char_logger, "Requesting characters {1}")(curried_getter)
-        listener = LoginListener(session, CharacterQueue(getter, push_db))
+        # Create dependencies of the listener
+        getter = toolz.curry(get_characters)(session)
+        queue = CharacterQueue(getter, push_db)
+        listener = LoginListener(session, queue)
+
+        # Logging tasks
         asyncio.create_task(log_db())
-        await listener.listen()
+
+        try:
+            await listener.listen()
+        except KeyboardInterrupt:
+            await listener.stop()
+            sys.exit(0)
 
 
 asyncio.run(main())
