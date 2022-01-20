52e3c0437a83fb85353ce18bf22df4a4f7b41acd
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-20 19:55:30 +0000

    No longer creating a new container each time

---
+++ dgraph/init_db.sh
@@ -0,0 +1,1 @@
+docker run -it -p 5080:5080 -p 6080:6080 -p 8080:8080 -p 9080:9080 -p 8000:8000 -v ~/dgraph:/dgraph --name dgraph dgraph/standalone:master
--- dgraph/run_db.sh
+++ dgraph/run_db.sh
@@ -1,1 +1,1 @@
-docker run -it -p 5080:5080 -p 6080:6080 -p 8080:8080 -p 9080:9080 -p 8000:8000 -v ~/dgraph:/dgraph --name dgraph dgraph/standalone:v21.03.0
+docker start dgraph
