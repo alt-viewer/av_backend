79d361817b4292f5fe57e1d3b0350270afef6075
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-22 07:34:35 +0000

    Exported DBCharacter

--- entities/__init__.py
+++ entities/__init__.py
@@ -1,7 +1,14 @@
-from entities.character import Character
+from entities.character import Character, DBCharacter
 from entities.faction import Factions
 from entities.server import Servers
 from entities.item import Item
 from entities.match import Match
 
-__all__ = ["Character", "Factions", "Servers", "Item", "Match"]
+__all__ = [
+    "Character",
+    "Factions",
+    "Servers",
+    "Item",
+    "Match",
+    "DBCharacter",
+]
