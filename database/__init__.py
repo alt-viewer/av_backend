8973c40f1f2ffbe142fce4a545d2060e6844d71c
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-24 04:27:04 +0000

    Batch queries

--- database/__init__.py
+++ database/__init__.py
@@ -1,7 +1,7 @@
 from database.push import push_db, log_db
 from database.db_client import client
-from database.push_char import push_chars
-from database.mutation import batch_mutate
+from database.mutations.push_char import push_chars
+from database.mutations.mutation import batch_mutate
 
 __all__ = [
     "push_db",
--- database/mutations/push_char.py
+++ database/mutations/push_char.py
@@ -5,7 +5,7 @@ from traceback import print_exc
 
 from database.db_client import client
 from entities import Character
-from database.mutation import batch_mutate
+from database.mutations.mutation import batch_mutate
 
 
 async def push_chars(chars: list[Character]) -> None:
