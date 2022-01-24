e31c420d0139c6226f8b39d5b25e17c42527053d
eilidh <56912943+SMC242@users.noreply.github.com>
2022-01-24 08:11:08 +0000

    exported log_task

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,8 @@
-from database.push import push_db, log_db
 from database.push_char import push_chars
 from database.transport import GQLTransport
 from database.filter_new import new_chars
 from database.converters.char_json import char_to_json, char_jsons
+from database.log_db import log_task
 
 __all__ = [
     "push_db",
@@ -12,4 +12,5 @@ __all__ = [
     "new_chars",
     "char_to_json",
     "char_jsons",
+    "log_task",
 ]
