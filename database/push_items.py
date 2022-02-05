f45b0c5f6643f8c23e5efa377f600cbda0fce95f
eilidh <56912943+SMC242@users.noreply.github.com>
2022-02-05 00:37:28 +0000

    Using converter

--- database/push_items.py
+++ database/push_items.py
@@ -2,8 +2,8 @@ from gql import gql
 from typing import Iterator
 import toolz.curried as toolz
 
-from entities import Item, DBItem
-from converters import convert_json
+from entities import Item
+from converters import convert_json, load_items
 from database.gql import GQLClient
 
 query = gql(
@@ -21,13 +21,11 @@ mutation addItems($items: [AddItemInput!]!) {
 )
 
 
-async def push_items(client: GQLClient, items: Iterator[Item]) -> list[str]:
+async def push_items(client: GQLClient, items: Iterator[Item]) -> list[Item]:
     """Add/update the given items in the database and return {uid, xid, last_recorded}"""
     res = await client.execute(query, variable_values={"items": convert_json(items)})
     return toolz.pipe(
         res,
         toolz.get_in(["addItem", "item"]),
-        toolz.map(
-            lambda i: DBItem(**i),
-        ),
+        load_items,
     )
