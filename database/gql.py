698450cdb7a6566f16f1af28882ac244775ed431
eilidh <56912943+SMC242@users.noreply.github.com>
2022-03-06 12:39:15 +0000

    Helper context manager

--- database/gql.py
+++ database/gql.py
@@ -1,7 +1,8 @@
 from gql.transport.aiohttp import AIOHTTPTransport
-from gql.client import AsyncClientSession
+from gql.client import AsyncClientSession, Client
 from aiohttp import ClientSession
 from typing import TypeAlias
+from contextlib import asynccontextmanager
 
 GQLClient: TypeAlias = AsyncClientSession
 
@@ -18,3 +19,20 @@ class GQLTransport(AIOHTTPTransport):
 
     async def close(self) -> None:
         pass
+
+
+@asynccontextmanager
+async def get_sessions(url: str):
+    """
+    Create an AIOHTTP session and a GQL client.
+    Returns (AIOHTTP session, GQL session)
+    """
+    try:
+        asession = ClientSession()
+        transport = GQLTransport(url, client_session=asession)
+        gsession = Client(transport=transport)
+        yield (asession, gsession)
+    finally:
+        await asession.close()
+        if gsession.transport:
+            await gsession.transport.close()
