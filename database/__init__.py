a33f7e8fe053549e432845572620f46a2f6f174c
eilidh <56912943+SMC242@users.noreply.github.com>
2022-03-06 12:39:25 +0000

    Added to export

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,7 @@
 from database.mutations.push_char import push_chars
-from database.gql import GQLTransport, GQLClient
+from database.gql import GQLTransport, GQLClient, get_sessions
 from database.queries.log_db import log_task
+from database.queries.match_chars_query import get_match_chars
 from database.mutations.push_items import push_items
 
 __all__ = [
@@ -9,4 +10,6 @@ __all__ = [
     "log_task",
     "push_items",
     "GQLClient",
+    "get_match_chars",
+    "get_sessions",
 ]
