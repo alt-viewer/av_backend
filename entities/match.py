724ad0f644b71de4f0535e4ce0f7bd4ffa48e23e
eilidh <56912943+SMC242@users.noreply.github.com>
2022-03-20 05:57:18 +0000

    New Match type

--- entities/match.py
+++ entities/match.py
@@ -1,11 +1,9 @@
 from dataclasses import dataclass
-from datetime import datetime
-from typing import List
 
-# TODO: this structure needs more thought
+from entities.character import MatchCharDict
+
+
 @dataclass
 class Match:
-    char1_id: int
-    char2_id: int
-    matched_at: datetime
-    confidence: int
+    peers: list[MatchCharDict]
+    confidence: float
