6fcb68fecd85014b212ed13d1372c9575c9e99e7
eilidh <56912943+SMC242@users.noreply.github.com>
2022-04-05 07:21:37 +0100

    Fixed identifier error

--- dgraph/wipe_db.sh
+++ dgraph/wipe_db.sh
@@ -1,14 +1,15 @@
 RED='\033[0;31m'
 NC='\033[0m'
 printf "This will ${RED}delete${NC} the whole Alt Viewer database. Are you sure?"
-read -n 1 -r
+read -r -n 1
 echo
+echo $REPLY
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	bash ./stop_db.sh
 	docker rm dgraph
 	echo "Deleted the Alt Viewer database."
-	read -p "Would you like to reinitialise the database?" -n 1 -r
+	read -p "Would you like to reinitialise the database?" -r -n 1
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
 		bash ./init_db.sh
