7a7b08aefc5481e447a4aafed4b8353562c1914a
eilidh <56912943+SMC242@users.noreply.github.com>
2023-04-04 12:20:40 +0100

    Removed dgraph

--- dgraph/init_db.sh
+++
@@ -1,1 +0,0 @@
-docker run -it -p 5080:5080 -p 6080:6080 -p 8080:8080 -p 9080:9080 -p 8000:8000 -v ~/dgraph:/dgraph --name dgraph dgraph/standalone:master
--- dgraph/load_schema.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST localhost:8080/admin/schema --data-binary '@schema.graphql'
