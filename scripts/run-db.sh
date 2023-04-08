ccfc767566ce63752c494ed915f433158bdf8b6e
eilidh <56912943+SMC242@users.noreply.github.com>
2023-04-08 14:27:42 +0100

    run-db now starts the docker daemon

--- scripts/run-db.sh
+++ scripts/run-db.sh
@@ -1,1 +1,6 @@
-docker start alt-viewer-mongo
+matches=$(ps -ef | grep -v grep | grep -c "dockerd")
+if [ $matches -eq 0 ]; then
+    echo "The docker daemon is not running. Starting docker..."
+    sudo systemctl start docker
+fi
+echo -n "Started " && docker start alt-viewer-mongo
