8cb9e91379704cf80346aa13e5c0ef827d8e0073
eilidh <56912943+SMC242@users.noreply.github.com>
2023-04-09 22:11:32 +0100

    Exported has_all

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,9 @@
 """Utils! I couldn't think of a clever name, so these are now eggs D:"""
-from utils.dict import omit, pick, replace_with
+from utils.dict import omit, pick, replace_with, has_all
 
 __all__ = [
     "omit",
     "replace_with",
     "pick",
+    "has_all",
 ]
