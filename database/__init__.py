3e87a1a1f944bc89851c37547298028990bc5792
eilidh <56912943+SMC242@users.noreply.github.com>
2023-04-09 22:15:09 +0100

    A separate query is no longer needed for inserting characters

--- database/__init__.py
+++ database/__init__.py
@@ -7,7 +7,6 @@ from database.queries.query_char import (
     get_char_by_xid,
     get_char_by_uid,
 )
-from database.mutations.push_items import push_items
 from database.mutations.insert_char import push_chars
 from database.types import DB, DBClient, Collection, XID, UID
 from database.paging import paged_collection
@@ -15,7 +14,6 @@ from database.paging import paged_collection
 __all__ = [
     "push_chars",
     "log_task",
-    "push_items",
     "match_char_pages",
     "get_sessions",
     "query",
