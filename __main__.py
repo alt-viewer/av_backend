4c8321384bd949ac9f62f1e31563745b657b839e
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-04-10 20:15:10 +0100

    Removed gql from main

--- __main__.py
+++ __main__.py
@@ -2,7 +2,6 @@ import aiohttp
 import asyncio
 import toolz.curried
 import sys
-import gql
 from toolz import curry
 from dotenv import load_dotenv
 
@@ -12,12 +11,10 @@ from census import get_characters
 from database import log_task, push_chars, get_sessions
 from logger import with_logger
 
-API_URL = "http://localhost:8080/graphql"
-
 
 async def main():
     load_dotenv()
-    async with get_sessions(API_URL) as (asession, db):
+    async with get_sessions() as (asession, db):
         # Create dependencies of the listener
         dispatch = event_reducer(asession, db)
         listener = LoginListener(asession, dispatch)
