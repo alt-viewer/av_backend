b7c8b24d715752e0f0ace42c016f24d6796c3f66
eilidh <56912943+SMC242@users.noreply.github.com>
2023-04-10 06:47:24 +0100

    No longer exporting RequestQueue

--- listener/__init__.py
+++ listener/__init__.py
@@ -1,5 +1,4 @@
 from listener.listen import LoginListener
-from listener.queue import RequestQueue
 from listener.dispatch import event_reducer, Dispatch
 from listener.subscribe import (
     subscription,
@@ -12,7 +11,6 @@ from listener.filter_item import is_account_wide
 
 __all__ = [
     "LoginListener",
-    "RequestQueue",
     "event_reducer",
     "Dispatch",
     "subscription",
