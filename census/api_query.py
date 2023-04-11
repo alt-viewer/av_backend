783f3e20cb102386f53291ac58cfed700f044cd9
eilidh <56912943+SMC242@users.noreply.github.com>
2023-04-11 05:19:00 +0100

    WIP: experimental census query factory

--- census/api_query.py
+++ census/api_query.py
@@ -2,6 +2,12 @@ from yarl import URL
 from logging import getLogger
 from dotenv import dotenv_values
 from typing import TypeAlias, Any
+from collections.abc import Callable, Iterable
+from functools import wraps
+import toolz.curried as toolz
+
+from entities import XID
+from entities.converters import Converter
 
 env_id = dotenv_values(".env").get("SERVICE_ID")
 if env_id is None:
@@ -34,3 +40,53 @@ def census_url(
         path=full_path,
         query=query,
     )
+
+
+def commas(xs: Iterable[Any]) -> str:
+    return ",".join(xs)
+
+
+Fields: TypeAlias = list[str]
+Joins: TypeAlias = list[str]
+ParamFactory: TypeAlias = Callable[[Fields, Joins], Params]
+
+
+def param_factory(default_fields: Fields, default_joins: Joins) -> ParamFactory:
+    return lambda fields, joins: {
+        "c:show": commas(fields or default_fields),
+        "c:resolve": commas(joins or default_joins),
+        "c:lang": "en",
+    }
+
+
+def with_ids(
+    factory: ParamFactory,
+) -> Callable[[str, Fields, Joins, list[XID]], Params]:
+    @wraps(factory)
+    @toolz.curry
+    def wrapper(
+        field_name: str, fields: Fields, joins: Joins, ids: list[XID]
+    ) -> Params:
+        return {**factory(fields, joins), field_name: commas(map(str, ids))}
+
+    return wrapper
+
+
+# TODO: A factory for census request functions.
+# ParamFactory -> Converter -> (ClientSession -> Optional[Fields] -> Optional[Joins] -> list[T])
+async def collection_query(make_params: ParamFactory, convert: Converter):
+    def make_query(path: str, fields: Fields, joins: Joins) -> URL:
+        return census_url(
+            path=path,
+            params=make_params(fields, joins),
+        )
+
+    async def do_query():
+        pass
+
+    return make_query
+
+
+# TODO: a wrapper around `collection_query` that wraps the factory using `with_ids`
+# allowing filtered requests.
+# ParamFactory -> Converter -> (ClientSession -> String -> Optional[Fields] -> Optional[Joins] -> [XID] -> list[T])
