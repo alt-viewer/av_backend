b12404d4dbdaa5762171b65bab1fea4fb9a954ad
eilidh <56912943+SMC242@users.noreply.github.com>
2023-04-12 03:20:23 +0100

    #14: Initial implementation of census_query

--- census/api_query.py
+++ census/api_query.py
@@ -1,13 +1,14 @@
 from yarl import URL
 from logging import getLogger
 from dotenv import dotenv_values
-from typing import TypeAlias, Any
-from collections.abc import Callable, Iterable
-from functools import wraps
+from typing import TypeAlias, Any, TypedDict, TypeVar
+from collections.abc import Callable, Iterable, Awaitable
+from functools import wraps, partial
 import toolz.curried as toolz
+from aiohttp import ClientSession, ClientResponse
 
 from entities import XID
-from entities.converters import Converter
+from entities.converters import Converter, with_conversion, JSON, JSONValue
 
 env_id = dotenv_values(".env").get("SERVICE_ID")
 if env_id is None:
@@ -25,6 +26,14 @@ def http_path(path: str | None, service_id: str) -> str:
 
 
 Params: TypeAlias = dict[str, str]
+Fields: TypeAlias = list[str]
+Joins: TypeAlias = list[str]
+FilterValue: TypeAlias = str | int | list
+Filter = TypedDict("Filter", {"field_name": str, "field_value": FilterValue})
+ParamFactory: TypeAlias = Callable[[Fields, Joins, list[Filter]], Params]
+T = TypeVar("T")
+
+query_logger = getLogger("census query")
 
 
 def _census_url(
@@ -52,47 +61,61 @@ def commas(xs: Iterable[Any]) -> str:
     return ",".join(xs)
 
 
-Fields: TypeAlias = list[str]
-Joins: TypeAlias = list[str]
-ParamFactory: TypeAlias = Callable[[Fields, Joins], Params]
+def convert_filter_value(value: FilterValue) -> str:
+    converters = {
+        str: toolz.identity,
+        int: str,
+        list: toolz.map(str),
+    }
+    t = type(value)
+    if t not in converters:
+        raise ValueError(f"Unhandled `FilterValue` type. Add a converter for {t}")
+    return converters[t](value)
 
 
 def param_factory(default_fields: Fields, default_joins: Joins) -> ParamFactory:
-    return lambda fields, joins: {
-        "c:show": commas(fields or default_fields),
-        "c:resolve": commas(joins or default_joins),
-        "c:lang": "en",
-    }
-
-
-def with_ids(
-    factory: ParamFactory,
-) -> Callable[[str, Fields, Joins, list[XID]], Params]:
-    @wraps(factory)
     @toolz.curry
-    def wrapper(
-        field_name: str, fields: Fields, joins: Joins, ids: list[XID]
-    ) -> Params:
-        return {**factory(fields, joins), field_name: commas(map(str, ids))}
+    def factory(fields: Fields, joins: Joins, filters: list[Filter]) -> Params:
+        return {
+            "c:lang": "en",
+            "c:show": commas(fields or default_fields),
+            "c:resolve": commas(joins or default_joins),
+        } | {f["field_name"]: convert_filter_value(f["field_value"]) for f in filters}
 
-    return wrapper
-
-
-# TODO: A factory for census request functions.
-# ParamFactory -> Converter -> (ClientSession -> Optional[Fields] -> Optional[Joins] -> list[T])
-async def collection_query(make_params: ParamFactory, convert: Converter):
-    def make_query(path: str, fields: Fields, joins: Joins) -> URL:
-        return census_url(
-            path=path,
-            params=make_params(fields, joins),
-        )
-
-    async def do_query():
-        pass
-
-    return make_query
+    return factory
 
 
-# TODO: a wrapper around `collection_query` that wraps the factory using `with_ids`
-# allowing filtered requests.
-# ParamFactory -> Converter -> (ClientSession -> String -> Optional[Fields] -> Optional[Joins] -> [XID] -> list[T])
+def result_of(path: str, json: JSON) -> list[JSON]:
+    """Get the results from a Census response based on its path"""
+    xs = json[f"{path}_list"]
+    if not isinstance(xs, list):
+        raise RuntimeError("Could not identify result list")
+    return xs
+
+
+def default_fail(r: ClientResponse) -> None:
+    query_logger.error(
+        f'Request for {r.url} failed with status {r.status} because "{r.reason}"'
+    )
+
+
+async def census_query(
+    path: str,
+    make_params: ParamFactory,
+    convert: Converter,
+    on_fail: Callable[[ClientResponse], None] | None = None,
+) -> Callable[[ClientSession, list[Filter]], Awaitable[T | None]]:
+    @toolz.curry
+    @with_conversion(convert)
+    async def query(
+        session: ClientSession, fields: Fields, joins: Joins, filters: list[Filter]
+    ) -> list[JSON] | None:
+        params = make_params(fields, joins, filters)
+        url = census_url(path, params)
+        async with session.get(url) as response:
+            if not response.ok:
+                return (on_fail or default_fail)(response)
+            json = await response.json()
+        return result_of(path, json)
+
+    return query
